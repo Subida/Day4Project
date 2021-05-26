f26a9c6e8c4dd85ddec6dffd84d70603cce6d31a
Reyimujiang Subidan <subidareyimujiang@gmail.com>
2021-05-25 21:19:18 -0400

    TC 001 has been completed

--- src/test/java/TestCase001.java
+++ src/test/java/TestCase001.java
@@ -1,4 +1,7 @@
 public class TestCase001 {
 //work1
     ///work2
+    //finishd this test case ready to luch to remore
+    //ready to open pull request
+    //discuss and eventually merge the code
 }
